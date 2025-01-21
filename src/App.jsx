60a951cf3d0d2a10ffec6d12cef7b789fb4cb4ef
Evander-byte <emondragon571@gmail.com>
2025-01-21 13:15:24 -0600

    First components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { Route, Routes } from "react-router"
+import Orders from "./components/pages/Orders"
+import Menu from "./components/pages/Menu"
+import NewDish from "./components/pages/NewDish"
+import SideBar from "./components/ui/SideBar"
 
 
 function App() {
 
   return (
-    <>
-      <h1 className='text-5xl font-light underline'>Hola Mundo</h1>
-    </>
+    <div>
+      <SideBar/>
+      <Routes>
+        <Route path="/" element={<Orders/>}/>
+        <Route path="/menu" element={<Menu/>}/>
+        <Route path="/new-dish" element={<NewDish/>}/>
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/components/ui/SideBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div className="md:w-2/5 xl:w-1/5 bg-gray-800">
+        <div className="p-6">
+            <p className="uppercase text-white text-2xl tracking-wide text-center font-bold">Restaurant app</p>
+        </div>
+    </div>
+  )
+}
+
+export default SideBar
